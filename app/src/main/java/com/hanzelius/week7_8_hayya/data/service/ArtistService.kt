136866044947d0290e23ea936956266601e14f4a
Infinite-cpu-ai <hanzelius01@student.ciputra.ac.id>
2025-10-30 12:27:22 +0700

    pembuatan file 2

--- app/src/main/java/com/hanzelius/week7_8_hayya/data/service/ArtistService.kt
+++ app/src/main/java/com/hanzelius/week7_8_hayya/data/service/ArtistService.kt
@@ -1,13 +1,24 @@
 package com.hanzelius.week7_8_hayya.data.service
 
+import com.hanzelius.week7_8_hayya.data.dto.AlbumDetailResponse
+import com.hanzelius.week7_8_hayya.data.dto.AlbumResponse
 import com.hanzelius.week7_8_hayya.data.dto.Artist
+import com.hanzelius.week7_8_hayya.data.dto.ArtistResponse
+import com.hanzelius.week7_8_hayya.data.dto.TrackResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ArtistService {
-    @GET("api/v1/json/123/search.php")
-    suspend fun getCurrentArtist(
-        @Query("s") artist: String,
-    ): Response<Artist>
+    @GET("search.php")
+    suspend fun searchArtist(@Query("s") artistName: String): Response<ArtistResponse>
+
+    @GET("searchalbum.php")
+    suspend fun searchAlbum(@Query("s") artistName: String): Response<AlbumResponse>
+
+    @GET("album.php")
+    suspend fun albumDetail(@Query("m") albumId: String): Response<AlbumDetailResponse>
+
+    @GET("track.php")
+    suspend fun albumTracks(@Query("m") albumId: String): Response<TrackResponse>
 }
