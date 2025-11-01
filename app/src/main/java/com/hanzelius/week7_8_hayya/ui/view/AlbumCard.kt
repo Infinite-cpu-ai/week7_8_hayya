6ce7f19dcc3d7720688c2a5d05efb80aab0a9e70
Infinite-cpu-ai <hanzelius01@student.ciputra.ac.id>
2025-11-01 07:18:16 +0700

    AlbumCard

--- app/src/main/java/com/hanzelius/week7_8_hayya/ui/view/AlbumCard.kt
+++ app/src/main/java/com/hanzelius/week7_8_hayya/ui/view/AlbumCard.kt
@@ -3,19 +3,23 @@ package com.hanzelius.week7_8_hayya.ui.view
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import com.hanzelius.week7_8_hayya.R
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.hanzelius.week7_8_hayya.ui.model.Album
 
 @Composable
@@ -38,13 +42,30 @@ fun AlbumCard(
         Column(
             horizontalAlignment = Alignment.Start
         ) {
-
+            AsyncImage(
+                model = album.albumThumb,
+                contentDescription = "album thumbnail",
+                modifier = Modifier.height(145.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .size(160.dp),
+                contentScale = ContentScale.Crop
+            )
+            Column (
+                modifier = modifier.padding(vertical = 12.dp)
+                    .padding(top = 12.dp)
+            ){
+                Text(
+                    text = album.albumName,
+                    fontSize = 14.sp,
+                    color = Color(0xFFC3BCA8),
+                    maxLines = 1
+                )
+                Text(
+                    text = "${album.albumYear} • ${album.albumGenre}",
+                    fontSize = 12.sp,
+                    color = Color(0xFFC3BCA8)
+                )
+            }
         }
     }
-}
-
-@Preview
-@Composable
-fun AlbumCardPreview() {
-    AlbumCard(R.drawable.ic_launcher_background, "Sob Rock", 2021, "Rock")
 }
