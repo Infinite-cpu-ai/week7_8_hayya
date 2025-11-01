f8d349445b87526e79f5178f8f2e70efbb7c977d
Infinite-cpu-ai <hanzelius01@student.ciputra.ac.id>
2025-11-01 08:27:47 +0700

    ErrorView

--- app/src/main/java/com/hanzelius/week7_8_hayya/MainActivity.kt
+++ app/src/main/java/com/hanzelius/week7_8_hayya/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.hanzelius.week7_8_hayya.ui.route.ArtistAppRoute
 import com.hanzelius.week7_8_hayya.ui.theme.Week7_8_HayyaTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Week7_8_HayyaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ArtistAppRoute()
             }
         }
     }
