eb07ce3216c88a977dce7854277cfeb1f3da39cb
Infinite-cpu-ai <hanzelius01@student.ciputra.ac.id>
2025-11-01 07:22:36 +0700

    LoadingView

--- app/src/main/java/com/hanzelius/week7_8_hayya/ui/view/LoadingView.kt
+++ app/src/main/java/com/hanzelius/week7_8_hayya/ui/view/LoadingView.kt
@@ -1,18 +1,25 @@
 package com.hanzelius.week7_8_hayya.ui.view
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun LoadingView(modifier: Modifier = Modifier) {
-    Box(
+    Column (
         modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        CircularProgressIndicator()
+        CircularProgressIndicator(
+            strokeWidth = 5.dp,
+            color = Color(0xFFD1B34B)
+        )
     }
 }
